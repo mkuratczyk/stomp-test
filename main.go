888f2a31e7c9e675c0884c4c426558bf2f0f0708
Michal Kuratczyk <mkuratczyk@vmware.com>
2023-09-07 16:19:51 +0200

    Add consumerCount and timestampBody

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/binary"
 	"flag"
 	"fmt"
 	"os"
@@ -13,9 +14,11 @@ const defaultPort = ":61613"
 
 var serverAddr = flag.String("server", "localhost:61613", "STOMP server endpoint")
 var messageCount = flag.Int("count", 10, "Number of messages to send/receive")
+var consumerCount = flag.Int("consumerCount", 1, "Number of consumers")
 var queueName = flag.String("queue", "/queue/stomp_test", "Destination queue")
 var publishOnly = flag.Bool("publishOnly", false, "If true, only publish messages, don't subscribe")
 var consumeOnly = flag.Bool("consumeOnly", false, "If true, only consume messages, don't publish")
+var timestampBody = flag.Bool("timestampBody", false, "If true, message body is perf-test compatible")
 var helpFlag = flag.Bool("help", false, "Print help text")
 var stop = make(chan bool)
 
@@ -34,12 +37,13 @@ func main() {
 	}
 
 	if !*publishOnly {
-		fmt.Fprintf(os.Stderr, "Subscribing..\n")
-		subscribed := make(chan bool)
-		go recvMessages(subscribed)
+		for i := 0; i < *consumerCount; i++ {
+			subscribed := make(chan bool)
+			go recvMessages(subscribed)
 
-		// wait until we know the receiver has subscribed
-		<-subscribed
+			// wait until we know the receiver has subscribed
+			<-subscribed
+		}
 	}
 
 	if !*consumeOnly {
@@ -64,9 +68,17 @@ func sendMessages() {
 	}
 
 	for i := 1; i <= *messageCount; i++ {
-		text := fmt.Sprintf("Message #%d", i)
-		err = conn.Send(*queueName, "text/plain",
+		var text string
+		if *timestampBody {
+			b := make([]byte, 12)
+			binary.BigEndian.PutUint32(b[0:], uint32(1234))
+			binary.BigEndian.PutUint64(b[4:], uint64(time.Now().UnixMilli()))
+			err = conn.Send(*queueName, "", b, nil)
+		} else {
+			text = fmt.Sprintf("Message #%d", i)
+			err = conn.Send(*queueName, "text/plain",
 			[]byte(text), nil)
+		}
 		if err != nil {
 			println("failed to send to server", err)
 			return
